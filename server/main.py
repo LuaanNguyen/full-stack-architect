f01a9413921c76d6add33d7a7c4226fd2ba9156f
LuaanNguyen <luanthiennguyen2003@gmail.com>
2024-08-13 11:32:30 -0600

    Added PATCH: update contact

--- server/main.py
+++ server/main.py
@@ -27,6 +27,23 @@ def create_contact():
         return jsonify({"message", str(e)}), 400
     
     return jsonify({"message": "Contact created."}), 201
+
+
+@app.route("/update_contact/<int:user_id>", methods=["PATCH"])
+def update_contact(user_id):
+    contact = Contact.query.get(user_id)
+
+    if not contact:
+        return jsonify({"message": "User not found"}), 404
+
+    data = request.json
+    contact.first_name = data.get("firstName", contact.first_name)
+    contact.last_name = data.get("lastName", contact.last_name)
+    contact.email = data.get("email", contact.email)
+
+    db.session.commit()
+
+    return jsonify({"message": "Usr updated."}), 200
     
 
 if __name__ == "__main__":
