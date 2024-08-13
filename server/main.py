3be9979fa185549431ec16589bdae58f7291724d
LuaanNguyen <luanthiennguyen2003@gmail.com>
2024-08-13 11:30:03 -0600

    Added POST: create contact

--- server/main.py
+++ server/main.py
@@ -6,8 +6,27 @@ from models import Contact
 def get_contacts():
     contacts = Contact.query.all() #get all different contacts 
     json_contacts = list(map(lambda x: x.to_json(), contacts))
-    return jsonify({"contacts": json_contacts})
+    return jsonify({"contacts": json_contacts}), 200
     
+    
+@app.route("/create_contact", methods=["POST"])
+def create_contact():
+    first_name = request.json.get("firstName")
+    last_name = request.json.get("lastName")
+    email = request.json.get("email")
+    
+    if not first_name and not last_name and not email:
+        return jsonify({"message": "Body must include first name, last name, and email"}), 400
+    
+    new_contact = Contact(first_name = first_name, last_name = last_name, email = email)
+    
+    try:
+        db.session.add(new_contact)
+        db.session.commit() 
+    except Exception as e:
+        return jsonify({"message", str(e)}), 400
+    
+    return jsonify({"message": "Contact created."}), 201
     
 
 if __name__ == "__main__":
